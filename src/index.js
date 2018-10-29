b00375579f684be48de2678df05ff0d8eda9fe28
VictoriaMeng <1ststarofthenight@gmail.com>
2018-10-29 15:11:34 -0700

    Initialize store variable with createSTore function. Pass store into Provider as a prop

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
+const store = createStore;
+
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>
 , document.getElementById('root'));
