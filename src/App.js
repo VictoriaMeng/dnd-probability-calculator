099e39d848aefb85cdd8a014bcb5e12268905d9c
VictoriaMeng <1ststarofthenight@gmail.com>
2018-10-29 16:40:55 -0700

    Import Container, Row, and Col components from reactstrap. Add boostrap column classes to two test columns in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap'
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <Container>
+        <Row>
+          <Col xs="2">Col 1</Col>
+          <Col xs="2">Col 2</Col>
+        </Row>
+      </Container>
     )
   }
 }
