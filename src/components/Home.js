cc880d2652a99d661998ca65bc02753e76baf53b
VictoriaMeng <1ststarofthenight@gmail.com>
2018-10-29 18:41:14 -0700

    Correct AC and ST Container directory links in import

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, Button } from 'reactstrap'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
-import ACContainer from './containers/ACContainer'
-import SavingThrowContainer from './containers/SavingThrowContainer'
-import Home from './components/Home'
+import ACContainer from '../containers/ACContainer'
+import SavingThrowContainer from '../containers/SavingThrowContainer'
 
-const Home = () => {
+const Home = () => 
+  <Container>
+  <Row>
+    <Col xl="6">
+      <Link to="/ac">Calculate AC</Link>
+    </Col>
 
-}
+    <Col xl="6">
+      <Link to="/saving-throw">Calculate Saving Throw</Link>
+    </Col>
+  </Row>
+</Container>
 
 export default Home;
