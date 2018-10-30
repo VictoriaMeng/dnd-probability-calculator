35272924c841410853200a31938555945529aa8f
VictoriaMeng <1ststarofthenight@gmail.com>
2018-10-29 17:14:32 -0700

    Import Button component from reactstrap. Change text in the two xl App columns to buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap'
+import { Container, Row, Col, Button } from 'reactstrap'
 
 export default class App extends Component {
   render() {
     return (
       <Container>
         <Row>
-          <Col xs="2">Calculate AC</Col>
-          <Col xs="2">Calculate Saving Throw</Col>
+          <Col xl="6">
+            <Button color="primary" size="lg">
+              Calculate AC
+            </Button>
+          </Col>
+
+          <Col xl="6">
+            <Button color="primary" size="lg">
+              Calculate Saving Throw
+            </Button>
+          </Col>
         </Row>
       </Container>
     )
