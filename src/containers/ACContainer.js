e547be3aedad4db89fc008f76ac56343deca3dae
VictoriaMeng <1ststarofthenight@gmail.com>
2018-10-30 11:04:39 -0700

    Import connect bundle and connect ACContainer to app's store

--- src/containers/ACContainer.js
+++ src/containers/ACContainer.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import ACInput from '../components/ACInput';
+import { connect } from 'react-redux';
 
-export default class ACContainer extends Component {
+class ACContainer extends Component {
   render() {
     return (
       <ACInput />
     )
   }
 }
+
+export default connect()(ACContainer)
