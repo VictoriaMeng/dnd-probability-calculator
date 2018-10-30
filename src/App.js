4dad546e7d21e37122fe173fd8399d9739eeb737
VictoriaMeng <1ststarofthenight@gmail.com>
2018-10-29 17:23:49 -0700

    Create ACContainer and SavingThrowContainer files and import the two new containers into App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, Button } from 'reactstrap'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import ACContainer from './containers/ACContainer'
+import SavingThrowContainer from './containers/SavingThrowContainer'
 
 export default class App extends Component {
   render() {
@@ -8,13 +10,13 @@ export default class App extends Component {
       <Container>
         <Row>
           <Col xl="6">
-            <Button color="primary" size="lg">
+            <Button color="link" size="lg">
               Calculate AC
             </Button>
           </Col>
 
           <Col xl="6">
-            <Button color="primary" size="lg">
+            <Button color="link" size="lg">
               Calculate Saving Throw
             </Button>
           </Col>
---
+++ src/containers/ACContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class ACContainer extends Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
