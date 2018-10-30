a84b1d06db838c1dee49b657f8b8d1b252a53252
VictoriaMeng <1ststarofthenight@gmail.com>
2018-10-29 20:03:42 -0700

    Fix incorrect React import in ACInput. Unlike Component, React should be outside of hash brackets since it's imported as a regular component instead of a bundle

--- src/components/ACInput.js
+++ src/components/ACInput.js
@@ -1,4 +1,4 @@
-import { React, Component } from 'react';
+import React, {Component } from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import { Container, Row, Col } from 'reactstrap';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
--- src/containers/ACContainer.js
+++ src/containers/ACContainer.js
@@ -4,7 +4,7 @@ import ACInput from '../components/ACInput';
 export default class ACContainer extends Component {
   render() {
     return (
-      <div>It's AC</div>
+      <ACInput />
     )
   }
 }
