46414f5f2f4823313d516293627432ae8e4b6aed
VictoriaMeng <1ststarofthenight@gmail.com>
2018-10-30 10:37:44 -0700

    Bugfix by deleting package-lock.json and running npm install to install correct version of webpackage. Add ACInput form w/ AC and modifier fields

--- src/components/ACInput.js
+++ src/components/ACInput.js
@@ -4,9 +4,20 @@ import { Container, Row, Col } from 'reactstrap';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export default class ACInput extends Component {
+  state = {
+    text: ''
+  }
+  
   render() {
     return (
-      <div>Input</div>
+      <Form inline>
+        <Label for="attack-modifier">Attack Modifier</Label>
+        <Input type="number" id="attack-modifier"></Input>
+        
+        <Label for="enemy-AC">Enemy AC</Label>
+        <Input type="number" id="enemy-AC"></Input>
+        <Button>Calculate Chance to Hit - AC</Button>
+      </Form>
     )
   }
 }
