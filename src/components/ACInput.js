444a6011582316ed9fa963d915ba596882de1fc2
VictoriaMeng <1ststarofthenight@gmail.com>
2018-10-30 10:43:43 -0700

    Add name, value, and onChange attributes to ACInput form input fields. Write code to change state in HandleChange function

--- src/components/ACInput.js
+++ src/components/ACInput.js
@@ -5,17 +5,24 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export default class ACInput extends Component {
   state = {
-    text: ''
+    attackMod: '',
+    enemyAC: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
   }
   
   render() {
     return (
       <Form inline>
         <Label for="attack-modifier">Attack Modifier</Label>
-        <Input type="number" id="attack-modifier"></Input>
+        <Input name="attackMod" onChange={this.handleChange} type="number" id="attack-modifier"></Input>
         
         <Label for="enemy-AC">Enemy AC</Label>
-        <Input type="number" id="enemy-AC"></Input>
+        <Input name="enemyAC" onChange={this.handleChange} type="number" id="enemy-AC"></Input>
         <Button>Calculate Chance to Hit - AC</Button>
       </Form>
     )
