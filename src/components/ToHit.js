4db1d3bad5a1148e25dda2de5ba9d3090f956299
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-07 20:58:28 -0800

    Import Card and ListGroup reactstrap components to render ToHit chance as a list in a card

--- src/components/ToHit.js
+++ src/components/ToHit.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
+import { ListGroup, ListGroupItem } from 'reactstrap';
 
 const ToHit = (props) => 
-  <div>yo</div>
-
+  <div>
+    <Card body>
+      <CardTitle>{props.calculation.hitChance}</CardTitle>
+      <CardText>
+        <ListGroup>
+          <ListGroupItem>Target Stat: {props.calculation.stat}</ListGroupItem>
+          <ListGroupItem>Modifier: +{props.calculation.modifier}</ListGroupItem>          
+          <ListGroupItem>Target Value: {props.calculation.target}</ListGroupItem>
+        </ListGroup>
+      </CardText>
+    </Card>
+  </div>
 
 export default ToHit;
