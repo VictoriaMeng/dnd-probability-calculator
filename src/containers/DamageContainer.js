64f2dc39ef95c492e429d053046eaebe6693c2a0
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-07 21:11:34 -0800

    Remove direct rendering of Damage component. Damage component is now rendered conditionally in displayDamage function if this.props.damageCalculations isn't emptied

--- src/containers/DamageContainer.js
+++ src/containers/DamageContainer.js
@@ -5,11 +5,19 @@ import { damage } from '../actions/calculatorActions'
 import { connect } from 'react-redux'
 
 class DamageContainer extends Component {
+
+  displayDamage = () => {
+    if (this.props.damageCalculations.length > 0) {
+      const calcs = this.props.damageCalculations
+      return <Damage calculation={calcs[calcs.length-1]} />
+    }
+  }
+
   render() {
     return (
       <div>
         <DamageInput calculateDamage={this.props.calculateDamage} />
-        <Damage />
+        {this.displayDamage()}
       </div>
     )
   }
