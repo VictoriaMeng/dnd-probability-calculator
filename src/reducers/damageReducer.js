b592661b6ec3fd3371d989c81179cf145b60a71b
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-07 19:04:37 -0800

    Calculate damageRange and averageDamage in damageReducer to initialize newCalc object and pass it into state

--- src/reducers/damageReducer.js
+++ src/reducers/damageReducer.js
@@ -1,5 +1,16 @@
 export default function damageReducer(state = [], action) {
   switch (action.type) {
+    case 'CALCULATE_DAMAGE':
+      const dieCount = action.formData.dieCount
+      const dieValue = action.formData.dieValue
+
+      const damageRange = `${dieCount}-${dieCount * dieValue}`
+      const averageDamage = (dieValue + 1) / 2 * dieCount
+      
+      const newCalc = { damageRange: damageRange, averageDamage: averageDamage }
+      debugger;
+      return [...state, newCalc]
+
     default:
       return state
   }
