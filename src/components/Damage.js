ecb16d966dcc714cbb74c19e740965f54b0aeb33
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-07 21:16:22 -0800

    Write Card with ListGroup components that display damageCalculation data

--- src/components/Damage.js
+++ src/components/Damage.js
@@ -3,6 +3,14 @@ import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } fr
 import { ListGroup, ListGroupItem } from 'reactstrap';
 
 const Damage = (props) => 
-  <div>Damage!</div>
+  <div>
+    <Card body>
+      <CardTitle>Die: {props.calculation.die}</CardTitle>
+      <ListGroup>
+        <ListGroupItem>Damage Range: {props.calculation.damageRange}</ListGroupItem>
+        <ListGroupItem>Average Damage: {props.calculation.averageDamage}</ListGroupItem>
+      </ListGroup>
+    </Card>
+  </div>
 
 export default Damage;
