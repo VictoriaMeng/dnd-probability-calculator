16c4589388ae94671a9b49ff8f77eaf1509a5404
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-07 18:44:01 -0800

    Pass calculateDamage as a prop into DamageInput component to be invoked in handleSubmit function

--- src/containers/DamageContainer.js
+++ src/containers/DamageContainer.js
@@ -6,7 +6,7 @@ import { connect } from 'react-redux'
 class DamageContainer extends Component {
   render() {
     return(
-      <DamageInput />
+      <DamageInput calculateDamage={this.props.calculateDamage} />
     )
   }
 }
