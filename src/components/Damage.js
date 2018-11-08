06b7f500a3a3b28cba0bd3bb193b3fe5f2b61de1
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-07 21:03:06 -0800

    Import React, Card, ListGroup to prepare to render a Damage component in a card-list format. Define/export Damage component with placeholder div tags

---
+++ src/components/Damage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
+import { ListGroup, ListGroupItem } from 'reactstrap';
+
+const Damage = (props) => 
+  <div>
+  </div>
+
+export default Damage;
