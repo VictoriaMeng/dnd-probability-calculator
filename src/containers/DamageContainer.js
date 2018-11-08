f57456cce04d5da92ea6414d286214ab8aed8acd
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-07 18:33:59 -0800

    Remove extra space break in DamageContainer. Delete unneeded import of react-route in ToHitInput

--- src/containers/DamageContainer.js
+++ src/containers/DamageContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import DamageInput from '../components/DamageInput'
 
 export default class DamageContainer extends Component {
+
   render() {
     return(
       <DamageInput />
