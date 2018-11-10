d473b5f8c4a443d8076b9767becb56a252ad8f8e
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-09 18:35:03 -0800

    Add state with blank default email and password keys. Write render method that returns placeholder div

---
+++ src/components/LoginInput.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class LoginInput extends Component {
+  state = {
+    email: "",
+    password: ""
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
