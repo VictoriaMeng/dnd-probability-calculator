b55a15a046aa24315d1635851439f4c5c0df5e87
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-10 21:57:52 -0800

    Add dice_count, die_value, modifer, average, max, and min damage columns so attributes can be stored in database

--- db/migrate/20181111055151_create_damage_calculations.rb
+++ db/migrate/20181111055151_create_damage_calculations.rb
@@ -1,7 +1,11 @@
 class CreateDamageCalculations < ActiveRecord::Migration[5.2]
   def change
     create_table :damage_calculations do |t|
-
+      t.integer :dice_count
+      t.integer :die_value
+      t.integer :average_damage
+      t.integer :max_damage
+      t.integer :min_damage
       t.timestamps
     end
   end
