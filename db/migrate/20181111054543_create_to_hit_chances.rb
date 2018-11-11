cf3f1b15dffedce7fed72ad7921a2cdd4a872bb8
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-10 21:49:10 -0800

    Write columns for target stat, value, and modifier in CreateToHitChances so attributes can be saved to database

--- db/migrate/20181111054543_create_to_hit_chances.rb
+++ db/migrate/20181111054543_create_to_hit_chances.rb
@@ -1,7 +1,9 @@
 class CreateToHitChances < ActiveRecord::Migration[5.2]
   def change
     create_table :to_hit_chances do |t|
-
+      t.string :target_stat
+      t.integer :target_value
+      t.integer :stat_modifier
       t.timestamps
     end
   end
