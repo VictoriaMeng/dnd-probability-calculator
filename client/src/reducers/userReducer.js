0a0c053560bb760a60a97225560b69eb0195891a
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-12 15:09:25 -0800

    Change FETCH_USERS action to FETCH_USER> Change FETCH_USERS action to FETCH_USER to update state so it only contains user fetched by get request

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,10 +1,10 @@
 const userReducer = (state = {loading: false, users: []}, action) => {
   switch (action.type) {
-    case 'FETCH_USERS':
+    case 'FETCH_USER':
       debugger;
       return {
         loading: false,
-        users: action.payload
+        users: [action.payload]
       }
     default:
       return state
