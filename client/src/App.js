42930340680a2ad1cc3ecdcf1665f2e048f65d78
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-11 19:40:50 -0800

    Remove all user route actions except create, since only create action needs to return json object

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ export default class App extends Component {
         <Route exact path="/" component={Home}/>
         <Route path='/to-hit' component={ToHitContainer}/>
         <Route path='/damage' component={DamageContainer}/>
-        <Route path='/register' render={props => <UserInput {...props} action="Register" />}/>
+        <Route path='/users/new' render={props => <UserInput {...props} action="Register" />}/>
         <Route path="/login" render={props => <UserInput {...props} action="Login" />}/>
       </div>
     )
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
+  resources :users, only: [:create]
   resources :to_hit_chances
   resources :damage_calculations
 end
