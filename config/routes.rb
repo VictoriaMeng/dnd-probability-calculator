c3f3834d80e81f756ee453a10cd24ccbfcc28b91
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-11 19:31:34 -0800

    Write restful route resources for users, hit, and damage calcs to set up for writing controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :to_hit_chances
+  resources :damage_calculations
 end
