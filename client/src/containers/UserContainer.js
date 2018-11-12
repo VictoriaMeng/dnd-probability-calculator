2e3938c0e9c73bf4380483431f871a4aa1420828
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-12 15:28:57 -0800

    Render placeholder UserContainer text Write placeholder text to UserContainer and add UserContainer to Route component with '/users/:id' show route. This sets up a basic UserContainer that renders correctly

--- client/src/containers/UserContainer.js
+++ client/src/containers/UserContainer.js
@@ -1,6 +1,10 @@
-import React, { Container } from 'react';
-import User from '../components/user';
+import React, { Component } from 'react';
+// import User from '../components/user';
 
 export default class UserContainer extends Component {
-
+  render() {
+    return (
+      <div>UserContainer!</div>
+    )
+  }
 }
