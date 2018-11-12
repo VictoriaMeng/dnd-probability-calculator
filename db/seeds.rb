ba5c6900db5694454d8f537b8b2ab95d4d996cc7
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-11 19:20:15 -0800

    Seed dummy hitChance as example of hitChance object and to confirm hitChance database is properly migrated

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 user = User.create(email: 'me@gmail.com', password_digest: 'xfewerfdsfdfig')
+hitChance = ToHitChance.create(
+  target_stat: 'AC',
+  target_value: 15,
+  stat_modifier: 8,
+  result: 0.5,
+  result_text: '50%',
+  user_id: 1
+)
