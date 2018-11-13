90aa8643d242e9a2d5a17e64ca77b17ecdd1ff16
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-12 16:14:39 -0800

    Connect UserContainer to store Import connect function from redux Invoke connect when exporting UserContainer This should connect UserContainere to redux store

--- client/src/containers/UserContainer.js
+++ client/src/containers/UserContainer.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import User from '../components/User';
+import { connect } from 'react-redux'
 
-export default class UserContainer extends Component {
+class UserContainer extends Component {
   render() {
     return (
       <User id={1} />
     )
   }
 }
+
+export default connect()(UserContainer)
