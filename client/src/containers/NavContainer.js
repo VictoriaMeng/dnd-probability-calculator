094d95dde33cdfb0565fb8c2c42eb60b06c178dc
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-14 18:19:51 -0800

    Map logout to props and pass to Nav child

--- client/src/containers/NavContainer.js
+++ client/src/containers/NavContainer.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 import Navigation from '../components/nav/Nav.js'
 import { connect } from 'redux'
+import { logout } from '../actions/userActions'
 
 class NavContainer extends Component {
   render() {
     return (
       <Navigation />
     )
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    logout: () => dispatch(logout())
   }
 }
 
