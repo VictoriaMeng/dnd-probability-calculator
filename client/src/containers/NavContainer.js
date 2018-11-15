a60e8b1a115e9bf74937a290a48c439c4db6827f
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-14 18:16:52 -0800

    Connect NavContainer to store Will allow logout dispatch action to be mapped to props

--- client/src/containers/NavContainer.js
+++ client/src/containers/NavContainer.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import Navigation from '../components/nav/Nav.js'
+import { connect } from 'redux'
 
-export default class NavContainer extends Component {
+class NavContainer extends Component {
   render() {
     return (
       <Navigation />
     )
   }
 }
+
+export default connect()(NavContainer)
