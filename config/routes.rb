46eee12736243852e595e0a4a8f902db9621e556
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-14 22:59:47 -0800

    Restrict hit chance resources to create and index Backend only needs to create hit chances and show index of existing ones

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:show, :create]
-  resources :to_hit_chances
+  resources :to_hit_chances, only [:create]
   resources :damage_calculations
   post '/login', to: 'sessions#create'
 end
