6a734935964037bc2babe5b51b8f721cd6d7ff6e
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-14 23:04:10 -0800

    Fix only: syntax when limiting hit chances

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:show, :create]
-  resources :to_hit_chances, only [:create]
+  resources :to_hit_chances, only: [:index, :create]
   resources :damage_calculations
   post '/login', to: 'sessions#create'
 end
