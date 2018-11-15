cefdd56c4b0b2ef0e303533a007d63908d118cff
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-14 18:03:11 -0800

    Render NavContainer in App to always show Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -8,14 +8,14 @@ import AuthInput from './components/auth/AuthInput'
 import AuthContainer from './containers/AuthContainer'
 import UserContainer from './containers/UserContainer'
 import { store } from './index'
-import Navigation from './components/Nav'
+import NavContainer from './containers/NavContainer'
 
 export default class App extends Component {
   
   render() {
     return (
       <div>
-        <Navigation />
+        <NavContainer />
         <Route exact path="/" component={Home}/>
         <Route path='/to-hit' component={ToHitContainer}/>
         <Route path='/damage' component={DamageContainer}/>
