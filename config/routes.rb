83f2755bafb636cc97c627df28194a1f99f56d36
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-17 15:52:39 -0800

    Make hit chance resources nested resources under users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show, :create]
-  resources :to_hit_chances, only: [:index, :create]
+  resources :users, only: [:show, :create] do 
+    resources :to_hit_chances, only: [:index, :create]
+  end
   resources :damage_calculations
   post '/login', to: 'sessions#create'
 end
