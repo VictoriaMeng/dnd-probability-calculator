9c9cfbb559679e68565749f599bbcf0e52ac7ff7
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-17 17:23:53 -0800

    Write method to calculate/save hit chance Write method to save results as both float and string

--- app/models/to_hit_chance.rb
+++ app/models/to_hit_chance.rb
@@ -1,3 +1,11 @@
 class ToHitChance < ActiveRecord::Base
   belongs_to :user
+
+  def calculate_result
+    # ((21–(TargetValue–Modifier)/20)*100
+    result = (21.0-(self.target.to_f-self.modifier.to_f))/20.0
+    self.result = result
+    self.result_text = '#{result * 100.0}%'
+    self
+  end
 end
