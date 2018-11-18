0b70c481a8a7b9dc390f6ff018d521c2b94a4255
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-17 20:14:37 -0800

    Add sessionReducer to rootReducer State now has access to unnested isLoggedIn boolean value

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import toHitReducer from './toHitReducer'
 import damageReducer from './damageReducer'
 import userReducer from './userReducer'
+import sessionReducer from './sessionReducer'
 
 const rootReducer = combineReducers(
   { 
     users: userReducer,
     hitChances: toHitReducer,
     damageCalculations: damageReducer,
+    session: sessionReducer
   }
 );
 
