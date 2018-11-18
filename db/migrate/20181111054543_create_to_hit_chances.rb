6db1b23f4980eac38a6c18ea963dafc574ee2a57
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-17 16:59:55 -0800

    Correct column types Target column should be integer, not string Stat column should be string, not integer

--- db/migrate/20181111054543_create_to_hit_chances.rb
+++ db/migrate/20181111054543_create_to_hit_chances.rb
@@ -1,8 +1,8 @@
 class CreateToHitChances < ActiveRecord::Migration[5.2]
   def change
     create_table :to_hit_chances do |t|
-      t.string :target
-      t.integer :stat
+      t.integer :target
+      t.string :stat
       t.integer :modifier
       t.float :result
       t.string :result_text
