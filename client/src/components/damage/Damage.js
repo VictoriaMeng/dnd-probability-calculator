2370157ca6ff429836189d3825aa0ea696950a37
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-18 18:52:36 -0800

    Correct key names in card to display damage

--- client/src/components/damage/Damage.js
+++ client/src/components/damage/Damage.js
@@ -5,10 +5,10 @@ import { ListGroup, ListGroupItem } from 'reactstrap';
 const Damage = (props) => 
   <div>
     <Card body>
-      <CardTitle>Die: {props.calculation.die}</CardTitle>
+      <CardTitle>Die: {props.calculation.die_value}</CardTitle>
       <ListGroup>
-        <ListGroupItem>Damage Range: {props.calculation.damageRange}</ListGroupItem>
-        <ListGroupItem>Average Damage: {props.calculation.averageDamage}</ListGroupItem>
+        <ListGroupItem>Damage Range: {props.calculation.min}-{props.calculation.max}</ListGroupItem>
+        <ListGroupItem>Average Damage: {props.calculation.average}</ListGroupItem>
       </ListGroup>
       <Button>Save Result!</Button>
     </Card>
