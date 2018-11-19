3ff3872a6c5baf2ae29f0875d92134b2920a913a
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-18 23:25:48 -0800

    Add LOAD_DAMAGE case to update state w/fetched damage

--- client/src/reducers/damageReducer.js
+++ client/src/reducers/damageReducer.js
@@ -1,8 +1,10 @@
 export default function damageReducer(state = [], action) {
   switch (action.type) {
     case 'POST_DAMAGE_SUCCESS':
-      debugger;
       return [...state, action.payload]
+    
+    case 'LOAD_DAMAGE':
+      return action.payload
 
     default:
       return state
