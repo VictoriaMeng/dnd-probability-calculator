68ecdbd2bac5ceae321ffa3ee4cca978ef8a33fa
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-18 17:04:23 -0800

    Make damage resources nested within users Only logged in users should be able to create/get calculations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:show, :create] do 
     resources :to_hit_chances, only: [:index, :create]
+    resources :damage_calculations, only: [:index, :create]
   end
-  resources :damage_calculations
   post '/login', to: 'sessions#create'
 end
