ff3fc9c0db990c7be56182c378ed23690cbcc997
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-18 21:19:02 -0800

    Render Average Damage in CardTitle List full dice description in second ListGroupItem

--- client/src/components/damage/Damage.js
+++ client/src/components/damage/Damage.js
@@ -5,10 +5,10 @@ import { ListGroup, ListGroupItem } from 'reactstrap';
 const Damage = (props) => 
   <div>
     <Card body>
-      <CardTitle>Die: {props.calculation.die_value}</CardTitle>
+      <CardTitle>Average Damage: {props.calculation.average}</CardTitle>
       <ListGroup>
         <ListGroupItem>Damage Range: {props.calculation.min}-{props.calculation.max}</ListGroupItem>
-        <ListGroupItem>Average Damage: {props.calculation.average}</ListGroupItem>
+        <ListGroupItem>Dice: {props.calculation.dice_count}d{props.calculation.die_value}</ListGroupItem>
       </ListGroup>
       <Button>Save Result!</Button>
     </Card>
