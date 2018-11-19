969f61419fb7a2e1685cc09d10aad3eb48187689
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-18 17:44:12 -0800

    Define damage_params private method for security Limits accepted params to two keys from nested damage object

--- app/controllers/damage_calculations_controller.rb
+++ app/controllers/damage_calculations_controller.rb
@@ -6,4 +6,10 @@ class DamageCalculationsController < ApplicationController
     binding.pry
   end
 
+  private 
+
+  def damage_params
+    params.require(:damage).permit(:die_value, :dice_count)
+  end
+
 end
