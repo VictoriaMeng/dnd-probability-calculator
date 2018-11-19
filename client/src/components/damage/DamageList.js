47dcc97847a1a656b8567a45d5012a119e08a383
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-18 21:09:33 -0800

    Define render fn w/ placeholder text

--- client/src/components/damage/DamageList.js
+++ client/src/components/damage/DamageList.js
@@ -2,5 +2,9 @@ import React, { Component } from 'react'
 import Damage from './Damage'
 
 export default class DamageList extends Component {
-
+  render() {
+    return(
+      <div>DamageList!</div>
+    )
+  }
 }
