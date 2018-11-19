a903c6bf49fe62871d5dcc520646228a3a011b7e
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-18 19:16:32 -0800

    Define renderToHit function Will iterate over hitChances prop and render each hit chance separately

--- client/src/components/toHit/ToHitList.js
+++ client/src/components/toHit/ToHitList.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
+import ToHit from './ToHit'
 
 export default class ToHitList extends Component {
+  renderToHit = () => {
+
+  }
+  
   render() {
     return <div>ToHitList!</div>
   }
