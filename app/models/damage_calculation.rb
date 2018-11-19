186d5da6e941e5405a9407bd963320cb2b921aba
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-18 18:11:07 -0800

    Add formulas to find average, min, and max functions

--- app/models/damage_calculation.rb
+++ app/models/damage_calculation.rb
@@ -2,14 +2,21 @@ class DamageCalculation < ActiveRecord::Base
   belongs_to :user
 
   def fill
+    self.find_average
+    self.find_max
+    self.find_min
+    self
   end
 
   def find_average
+    (self.die_value + 1) / 2
   end
 
   def find_min
+    self.die_count
   end
 
   def find_max
+    self.die_value * self.die_count
   end
 end
