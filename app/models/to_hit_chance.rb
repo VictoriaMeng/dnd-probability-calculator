cdfb88516fc8d90d5ec33b23a4e3f98d19f1df56
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-18 16:22:01 -0800

    Split calculate_result into two separate methods Calculate_ac should be used if @hit_chance stat is AC Calculate_saving_throw should be used for all other stats

--- app/models/to_hit_chance.rb
+++ app/models/to_hit_chance.rb
@@ -1,10 +1,17 @@
 class ToHitChance < ActiveRecord::Base
   belongs_to :user
 
-  def calculate_result
+  def calculate_ac
     # ((21–(TargetValue–Modifier)/20)*100
     result = (21.0-(self.target.to_f-self.modifier.to_f))/20.0
     self.result = result
+    self.result_text = "#{result * 100.0}%"
+    self
+  end
+
+  def calculate_saving_throw
+    result = 1.0-((21.0-(self.target.to_f-self.modifier.to_f))/20.0)
+    self.result = result 
     self.result_text = "#{result * 100.0}%"
     self
   end
