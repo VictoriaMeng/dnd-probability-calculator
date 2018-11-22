bdac025725772c7cbff1ba5e29292a05079c9cb2
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-21 18:39:50 -0800

    Combine errorReducer in rootReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,13 +3,15 @@ import toHitReducer from './toHitReducer'
 import damageReducer from './damageReducer'
 import userReducer from './userReducer'
 import sessionReducer from './sessionReducer'
+import errorReducer from './errorReducer'
 
 const rootReducer = combineReducers(
   { 
     users: userReducer,
     hitChances: toHitReducer,
     damageCalculations: damageReducer,
-    session: sessionReducer
+    session: sessionReducer,
+    error: errorReducer
   }
 );
 
