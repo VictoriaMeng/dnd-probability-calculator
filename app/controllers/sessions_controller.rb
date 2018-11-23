037530027ab8de923dbc84f1c8f7a8a169a574d6
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-23 06:38:03 -0800

    Return login error message from backend Define conditional error stringifying functions Based on whether they should be simple or complex sentences

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,8 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:user][:email])
     if @user && @user.authenticate(params[:user][:password])
       render json: @user 
+    else
+      render json: {login: ['email/password combination invalid']}, status: 400
     end
   end
 end
--- client/src/containers/ErrorContainer.js
+++ client/src/containers/ErrorContainer.js
@@ -15,9 +15,21 @@ class ErrorContainer extends Component {
   stringifyErrors = () => {
     return Object.entries(this.props.errors)
     .map(e => {
-      return `${e[0].charAt(0).toUpperCase() + e[0].slice(1)} ${e[1].slice(0, -1).join(', ')} and ${e[1].slice(-1)}`
+      if (e[1].length > 1) {
+        return this.multipleErrors(e)
+      } else {
+        return this.singleErrors(e)
+      }
     }) 
     .join(". ")
+  }
+
+  singleErrors = (e) => {
+    return `${e[0].charAt(0).toUpperCase() + e[0].slice(1)} ${e[1].slice(0, -1).join(', ')} ${e[1].slice(-1)}`
+  }
+
+  multipleErrors = (e) => {
+    return `${e[0].charAt(0).toUpperCase() + e[0].slice(1)} ${e[1].slice(0, -1).join(', ')} and ${e[1].slice(-1)}`
   }
 
   renderErrors = () => {
