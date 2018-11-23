10c9719aa1294341acf44f75e15edb8d875a015b
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-23 08:44:30 -0800

    Add CLEAR_ERRORS case that resets state

--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -2,6 +2,8 @@ export default function errorReducer(state = {}, action) {
   switch (action.type) {
     case 'HANDLE_ERROR':
       return action.payload
+    case 'CLEAR_ERRORS':
+      return {}
     default:
       return state
   }
