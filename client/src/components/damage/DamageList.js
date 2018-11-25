0a1eb78273f442577e003344a87dfcaef058acf9
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-24 21:52:52 -0800

    Reverse damage array so newest calc renders first For user convenience

--- client/src/components/damage/DamageList.js
+++ client/src/components/damage/DamageList.js
@@ -3,7 +3,7 @@ import Damage from './Damage'
 
 export default class DamageList extends Component {
   renderDamage = () => {
-    return this.props.damageCalculations.map(d => {
+    return this.props.damageCalculations.reverse().map(d => {
       return <Damage calculation={d} />
     })
   }
