a498ae525ff5d032f8e8f21f4de83292386e59d1
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-24 19:32:38 -0800

    Add newPost boolean to damageReducer Will be checked in DamageContainer to see if new damage calc has been posted

--- client/src/reducers/damageReducer.js
+++ client/src/reducers/damageReducer.js
@@ -1,10 +1,19 @@
-export default function damageReducer(state = [], action) {
+export default function damageReducer(state = {
+  newPost: false,
+  data: []
+}, action) {
   switch (action.type) {
     case 'POST_DAMAGE_SUCCESS':
-      return [...state, action.payload]
+      return {
+        data: [...state.data, action.payload],
+        newPost: true
+      }
     
     case 'LOAD_DAMAGE':
-      return action.payload
+      return {
+        data: action.payload,
+        newPost: false
+      }
 
     default:
       return state
