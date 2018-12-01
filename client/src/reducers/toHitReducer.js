aa78c1bb388c0a0a120ec1ec05e7bdd00f4b9eb5
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-30 21:03:52 -0800

    Remove debuggers/unused cases from toHitReducer

--- client/src/reducers/toHitReducer.js
+++ client/src/reducers/toHitReducer.js
@@ -3,36 +3,17 @@ export default function toHitReducer(state = {
   data: []
 }, action) {
   switch (action.type) {
-    case 'POST_HIT_CHANCE_SUCCESS': 
-      debugger;     
+    case 'POST_HIT_CHANCE_SUCCESS':      
       return {
         data: [...state.data, action.payload],
         newPost: true
       }
     
     case 'LOAD_HIT_CHANCES':
-      debugger;
       return {
         data: action.payload,
         newPost: false
       }
-
-    case 'CALCULATE_SAVING_THROW_CHANCE':
-
-      const DC = action.formData.target
-      const statMod = action.formData.modifier
-
-      const failureChance = (1-((21-(DC-statMod))/20))
-    
-      const failureChanceObject = { 
-        stat: action.formData.stat,
-        target: DC,
-        modifier: statMod,
-        chance: failureChance,
-        chanceText: `${failureChance * 100}%`
-      }
-
-      return [...state, failureChanceObject]
 
     default:
       return state
