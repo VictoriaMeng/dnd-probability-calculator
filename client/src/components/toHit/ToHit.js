6b365e6f58c5132c2d31a158d9027b885daa3c1c
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-30 20:22:10 -0800

    Delete '+' sign from ToHit card The '+' sign doesn't apply to negative modifiers

--- client/src/components/toHit/ToHit.js
+++ client/src/components/toHit/ToHit.js
@@ -9,7 +9,7 @@ const ToHit = (props) =>
       <CardText>
         <ListGroup>
           <ListGroupItem>Target Stat: {props.calculation.stat}</ListGroupItem>
-          <ListGroupItem>Modifier: +{props.calculation.modifier}</ListGroupItem>          
+          <ListGroupItem>Modifier: {props.calculation.modifier}</ListGroupItem>          
           <ListGroupItem>Target Value: {props.calculation.target}</ListGroupItem>
         </ListGroup>
       </CardText>
