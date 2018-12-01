4a044c75fdcab87c371fa415f3227f84fb261fbb
VictoriaMeng <1ststarofthenight@gmail.com>
2018-11-30 20:51:32 -0800

    Delete hasNewErrors key from error state Never invoked in rest of app

--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -1,14 +1,12 @@
-export default function errorReducer(state = {messages: {}, hasNewErrors: false}, action) {
+export default function errorReducer(state = {messages: {}}, action) {
   switch (action.type) {
     case 'HANDLE_ERROR':
       return {
         messages: action.payload,
-        hasNewErrors: true
       }
     case 'CLEAR_ERRORS':
       return {
         messages: {},
-        hasNewErrors: false
       }
     default:
       return state
