af6521220cd49b66bd3a0f8c541df673ee21c8d8
VictoriaMeng <1ststarofthenight@gmail.com>
2018-12-03 19:57:56 -0800

    Add Knight logo to Home.js By adding figure node with .logo class to Home.js Then adding content url to .logo class in style.css

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 
 const Home = () => 
 <Container>
+<figure class="logo animated fadeInDown delay-07s" />
 </Container>
 
 export default Home;
