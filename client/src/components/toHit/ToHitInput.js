87f28ea708e38f47f60e913aaf03b8b990d5f6a5
VictoriaMeng <1ststarofthenight@gmail.com>
2018-12-03 23:21:34 -0800

    Add .col-xs-6 bootstrap attr to ToHitInput Easy way of making to hit form a smaller size

--- client/src/components/toHit/ToHitInput.js
+++ client/src/components/toHit/ToHitInput.js
@@ -45,29 +45,33 @@ export default class ToHitInput extends Component {
   
   render() {
     return (
-      <div>
-      {this.renderRedirect()}
-      <Form onSubmit={this.handleSubmit}>
-        <Label for="stat">Target Stat</Label>
-        <Input type="select" name="stat" onInput={this.handleChange} id="target-stat">
-          <option>AC</option>
-          <option>Strength</option>
-          <option>Dexterity</option>
-          <option>Constitution</option>
-          <option>Intelligence</option>
-          <option>Wisdom</option>
-          <option>Charisma</option>
-        </Input>
+      <Container>
+        <Row>
+          <Col xs='6'>
+            {this.renderRedirect()}
+            <Form className='form' onSubmit={this.handleSubmit}>
+              <Label for="stat">Target Stat</Label>
+              <Input className='input-text' type="select" name="stat" onInput={this.handleChange} id="target-stat">
+                <option>AC</option>
+                <option>Strength</option>
+                <option>Dexterity</option>
+                <option>Constitution</option>
+                <option>Intelligence</option>
+                <option>Wisdom</option>
+                <option>Charisma</option>
+              </Input>
 
-        <Label for="modifier">Modifier Value</Label>
-        <Input name="modifier" onChange={this.handleChange} type="number" id="modifier-value"></Input>
-        
-        <Label for="target">Target AC or DC</Label>
-        <Input name="target" onChange={this.handleChange} type="number" id="target-value"></Input>
+              <Label for="modifier">Modifier Value</Label>
+              <Input className='input-text' name="modifier" onChange={this.handleChange} type="number" id="modifier-value"></Input>
+              
+              <Label for="target">Target AC or DC</Label>
+              <Input className='input-text' name="target" onChange={this.handleChange} type="number" id="target-value"></Input>
 
-        <Input type="submit" value="Calculate"></Input>
-      </Form>
-      </div>
+              <Input className='input-btn calc' type="submit" value="Calculate"></Input>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     )
   }
 }
