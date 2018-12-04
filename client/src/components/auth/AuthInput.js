96b58cdd15c4a9f5ce86b2cbe171ab0210cf7ad6
VictoriaMeng <1ststarofthenight@gmail.com>
2018-12-04 13:55:54 -0800

    Wrap ErrorList in Container/Rol/Col From the reactstrap component library To take advantage of reactstrap Layout library if needed

--- client/src/components/auth/AuthInput.js
+++ client/src/components/auth/AuthInput.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Row, Col, Container, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 // import { handleChange } from '../../helpers/formHelpers'
 
 export default class AuthInput extends Component {
@@ -27,15 +27,19 @@ export default class AuthInput extends Component {
     return (
       <Container>
         <h2>{this.props.action}</h2>
-        <Form className='form' inline onSubmit={this.handleSubmit}>
-          <Label for="email">Email:</Label>
-          <Input className='input-text' onChange={this.handleChange} type="text" name="email" id="email"/>
-          
-          <Label for="Password">Password: </Label>
-          <Input className='input-text' onChange={this.handleChange} type="password" name="password" id="password"/>
-          
-          <Input className='input-btn' type="submit" value={this.props.action}/>
-        </Form>
+        <Row>
+          <Col>
+            <Form className='form' inline onSubmit={this.handleSubmit}>
+              <Label for="email">Email:</Label>
+              <Input className='input-text' onChange={this.handleChange} type="text" name="email" id="email"/>
+              
+              <Label for="Password">Password: </Label>
+              <Input className='input-text' onChange={this.handleChange} type="password" name="password" id="password"/>
+              
+              <Input className='input-btn' type="submit" value={this.props.action}/>
+            </Form>
+          </Col>
+        </Row>
       </Container>
     )
   }
