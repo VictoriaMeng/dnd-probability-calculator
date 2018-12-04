3e2abf540a250753e13e4fb282cf41d4e866165c
VictoriaMeng <1ststarofthenight@gmail.com>
2018-12-03 20:01:23 -0800

    Add h1 title to homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,7 +4,8 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 
 const Home = () => 
 <Container>
-<figure class="logo animated fadeInDown delay-07s" />
+  <figure class="logo animated fadeInDown delay-07s" />
+  <h1 class="animated fadeInDown delay-07s">5e Dungeons & Dragons Probability Calculator</h1>
 </Container>
 
 export default Home;
