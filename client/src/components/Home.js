0f1943cc77a99f267bc6e708b2c18e15695ec02b
VictoriaMeng <1ststarofthenight@gmail.com>
2018-12-06 12:10:10 -0800

    Delete commented out text

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 const Home = () => 
 <Container>
   <h1 class="animated fadeInDown delay-07s">5e Dungeons & Dragons Probability Calculator</h1>
-  {/* <a class="link animated fadeInUp delay-1s servicelink" href="#service">Get Started</a> */}
   <Link class='link animated fadeInUp delay-1s servicelink' to='/signup'>Sign Up</Link>
   <Link class='link animated fadeInUp delay-1s servicelink' to='/login'>Login</Link>
 </Container>
--- client/src/components/nav/PrivateNav.js
+++ client/src/components/nav/PrivateNav.js
@@ -1,34 +1,36 @@
 import React from 'react'
-import { Nav, NavItem, NavLink } from 'reactstrap'
+import { Nav, NavItem, NavLink, Container } from 'reactstrap'
 import { Link } from 'react-router-dom'
 
 const PrivateNav = (props) => 
-  <Nav>
-    <NavItem>
-      <NavLink tag={Link} to="/to-hit">
-        Calculate To Hit/Saving Throw Chance
-      </NavLink>
-    </NavItem>
+  <Container>
+    <Nav>
+      <NavItem>
+        <NavLink tag={Link} to="/to-hit">
+          Calculate To Hit/Saving Throw Chance
+        </NavLink>
+      </NavItem>
 
-    <NavItem>
-      <NavLink tag={Link} to ="/damage">
-        Calculate Damage
-      </NavLink>
-    </NavItem>
+      <NavItem>
+        <NavLink tag={Link} to ="/damage">
+          Calculate Damage
+        </NavLink>
+      </NavItem>
 
-    <NavItem>
-      <NavLink tag={Link} to={{
-        pathname: `/users/${sessionStorage.getItem('id')}`,
-      }}>
-        Your Page
-      </NavLink>
-    </NavItem>
-    
-    <NavItem>
-      <NavLink tag={Link} to="/" onClick={props.logout}>
-        Logout
-      </NavLink>
-    </NavItem>
-  </Nav>
+      <NavItem>
+        <NavLink tag={Link} to={{
+          pathname: `/users/${sessionStorage.getItem('id')}`,
+        }}>
+          Your Page
+        </NavLink>
+      </NavItem>
+
+      <NavItem>
+        <NavLink tag={Link} to="/" onClick={props.logout}>
+          Logout
+        </NavLink>
+      </NavItem>
+    </Nav>
+  </Container>
 
 export default PrivateNav
