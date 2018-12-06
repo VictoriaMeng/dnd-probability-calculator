d5162ce32a569ad26a91517af416ee4c43cea80c
VictoriaMeng <1ststarofthenight@gmail.com>
2018-12-06 15:09:08 -0800

    Delete <Container>. Children already wrapped in <Nav>

--- client/src/components/nav/PrivateNav.js
+++ client/src/components/nav/PrivateNav.js
@@ -1,36 +1,34 @@
 import React from 'react'
-import { Nav, NavItem, NavLink, Container } from 'reactstrap'
+import { Nav, NavItem, NavLink } from 'reactstrap'
 import { Link } from 'react-router-dom'
 
 const PrivateNav = (props) => 
-  <Container>
-    <Nav>
-      <NavItem>
-        <NavLink tag={Link} to="/to-hit">
-          Calculate To Hit/Saving Throw Chance
-        </NavLink>
-      </NavItem>
+  <Nav>
+    <NavItem>
+      <NavLink tag={Link} to="/to-hit">
+        Calculate To Hit/Saving Throw Chance
+      </NavLink>
+    </NavItem>
 
-      <NavItem>
-        <NavLink tag={Link} to ="/damage">
-          Calculate Damage
-        </NavLink>
-      </NavItem>
+    <NavItem>
+      <NavLink tag={Link} to ="/damage">
+        Calculate Damage
+      </NavLink>
+    </NavItem>
 
-      <NavItem>
-        <NavLink tag={Link} to={{
-          pathname: `/users/${sessionStorage.getItem('id')}`,
-        }}>
-          Your Page
-        </NavLink>
-      </NavItem>
+    <NavItem>
+      <NavLink tag={Link} to={{
+        pathname: `/users/${sessionStorage.getItem('id')}`,
+      }}>
+        Your Page
+      </NavLink>
+    </NavItem>
 
-      <NavItem>
-        <NavLink tag={Link} to="/" onClick={props.logout}>
-          Logout
-        </NavLink>
-      </NavItem>
-    </Nav>
-  </Container>
+    <NavItem>
+      <NavLink tag={Link} to="/" onClick={props.logout}>
+        Logout
+      </NavLink>
+    </NavItem>
+  </Nav>
 
 export default PrivateNav
