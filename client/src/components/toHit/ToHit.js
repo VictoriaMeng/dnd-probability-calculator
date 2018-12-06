033c197a2fa215fcf3d0d54f507020982754a18e
VictoriaMeng <1ststarofthenight@gmail.com>
2018-12-06 14:08:31 -0800

    Delete CardText from ToHit so <ul> won't be in <div>

--- client/src/components/toHit/ToHit.js
+++ client/src/components/toHit/ToHit.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { Card, CardText, CardTitle } from 'reactstrap';
+import { Card, CardTitle } from 'reactstrap';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 
 const ToHit = (props) => 
   <div>
     <Card body className='tile'>
       <CardTitle className='card-header'>Success Chance: {props.calculation.result_text}</CardTitle>
-      <CardText>
-        <ListGroup className='card-list'>
-          <ListGroupItem>Target Stat: {props.calculation.stat}</ListGroupItem>
-          <ListGroupItem>Modifier: {props.calculation.modifier}</ListGroupItem>          
-          <ListGroupItem>Target Value: {props.calculation.target}</ListGroupItem>
-        </ListGroup>
-      </CardText>
+      <ListGroup className='card-list'>
+        <ListGroupItem>Target Stat: {props.calculation.stat}</ListGroupItem>
+        <ListGroupItem>Modifier: {props.calculation.modifier}</ListGroupItem>          
+        <ListGroupItem>Target Value: {props.calculation.target}</ListGroupItem>
+      </ListGroup>
     </Card>
   </div>
 
