1b8d7a1f5271f69cae19602aff556369eb8dd63e
VictoriaMeng <1ststarofthenight@gmail.com>
2018-12-06 13:48:29 -0800

    Change 'class' to 'className' in Home <Link> nodes Selector className does not throw browser error in console

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,8 +5,8 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 const Home = () => 
 <Container>
   <h1>5e Dungeons & Dragons Probability Calculator</h1>
-  <Link class='link' to='/signup'>Sign Up</Link>
-  <Link class='link' to='/login'>Login</Link>
+  <Link className='link' to='/signup'>Sign Up</Link>
+  <Link className='link' to='/login'>Login</Link>
 </Container>
 
 export default Home;
