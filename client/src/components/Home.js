bf82c2d24c0352f76c98d90299d5373db333b6a2
VictoriaMeng <1ststarofthenight@gmail.com>
2018-12-06 13:21:42 -0800

    Delete unused selectors in Home.js Delete commented out text from style.css

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,9 +4,9 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 
 const Home = () => 
 <Container>
-  <h1 class="animated fadeInDown delay-07s">5e Dungeons & Dragons Probability Calculator</h1>
-  <Link class='link animated fadeInUp delay-1s servicelink' to='/signup'>Sign Up</Link>
-  <Link class='link animated fadeInUp delay-1s servicelink' to='/login'>Login</Link>
+  <h1>5e Dungeons & Dragons Probability Calculator</h1>
+  <Link class='link' to='/signup'>Sign Up</Link>
+  <Link class='link' to='/login'>Login</Link>
 </Container>
 
 export default Home;
