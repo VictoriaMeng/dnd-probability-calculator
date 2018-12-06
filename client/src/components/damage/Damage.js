282c938ef4c5c21e86e1f73fd1f96c05a959a368
VictoriaMeng <1ststarofthenight@gmail.com>
2018-12-06 15:03:35 -0800

    Delete empty Container from Damage.js

--- client/src/components/damage/Damage.js
+++ client/src/components/damage/Damage.js
@@ -3,14 +3,12 @@ import { Container, Card, CardTitle } from 'reactstrap';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 
 const Damage = (props) => 
-  <Container>
-    <Card body className='tile'>
-      <CardTitle className='card-header'>Average Damage: {props.calculation.average}</CardTitle>
-      <ListGroup className='card-list'>
-        <ListGroupItem>Damage Range: {props.calculation.min}-{props.calculation.max}</ListGroupItem>
-        <ListGroupItem>Dice: {props.calculation.dice_count}d{props.calculation.die_value}</ListGroupItem>
-      </ListGroup>
-    </Card>
-  </Container>
+  <Card body className='tile'>
+    <CardTitle className='card-header'>Average Damage: {props.calculation.average}</CardTitle>
+    <ListGroup className='card-list'>
+      <ListGroupItem>Damage Range: {props.calculation.min}-{props.calculation.max}</ListGroupItem>
+      <ListGroupItem>Dice: {props.calculation.dice_count}d{props.calculation.die_value}</ListGroupItem>
+    </ListGroup>
+  </Card>
 
 export default Damage;
