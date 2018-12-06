4c919cac4da2ab372c79ecef2f9e7e8c7aec8683
VictoriaMeng <1ststarofthenight@gmail.com>
2018-12-06 15:11:22 -0800

    Delete empty <div>. Children of ToHit already wrapped in <Card>

--- client/src/components/toHit/ToHit.js
+++ client/src/components/toHit/ToHit.js
@@ -3,15 +3,14 @@ import { Card, CardTitle } from 'reactstrap';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 
 const ToHit = (props) => 
-  <div>
-    <Card body className='tile'>
-      <CardTitle className='card-header'>Success Chance: {props.calculation.result_text}</CardTitle>
-      <ListGroup className='card-list'>
-        <ListGroupItem>Target Stat: {props.calculation.stat}</ListGroupItem>
-        <ListGroupItem>Modifier: {props.calculation.modifier}</ListGroupItem>          
-        <ListGroupItem>Target Value: {props.calculation.target}</ListGroupItem>
-      </ListGroup>
-    </Card>
-  </div>
+  <Card body className='tile'>
+    <CardTitle className='card-header'>Success Chance: {props.calculation.result_text}</CardTitle>
+    <ListGroup className='card-list'>
+      <ListGroupItem>Target Stat: {props.calculation.stat}</ListGroupItem>
+      <ListGroupItem>Modifier: {props.calculation.modifier}</ListGroupItem>          
+      <ListGroupItem>Target Value: {props.calculation.target}</ListGroupItem>
+    </ListGroup>
+  </Card>
+
 
 export default ToHit;
